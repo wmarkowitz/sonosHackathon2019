7478f6a4662c24d15e943c8c5bc49fc83134166e
Yong Emily Dich <yongemilydich@Yongs-MacBook-Pro.local>
2019-10-04 21:17:19 -0400

    relative vol

--- pythonTest.py
+++ pythonTest.py
@@ -1,7 +1,9 @@
 import requests
 import json
 import asyncio
-
+bedroom = "RINCON_7828CAE736E601400"
+bathroom = "RINCON_7828CAF4D45E01400"
+# bathroom
 async def lowerVolume():
     url = "https://api.ws.sonos.com/control/api/v1/players/RINCON_7828CAF4D45E01400/playerVolume"
 
@@ -21,6 +23,7 @@ async def lowerVolume():
     print(response.text)
     return(response.text)
 
+# bedroom
 async def raiseVolume():
     url = "https://api.ws.sonos.com/control/api/v1/players/RINCON_7828CAE736E601400/playerVolume"
 
@@ -38,8 +41,8 @@ async def raiseVolume():
     print(response.text)
     return(response.text)
 
-async def raiseRelativeVolume(volDelta):
-    url = "https://api.ws.sonos.com/control/api/v1/players/RINCON_7828CAE736E601400/playerVolume/relative"
+async def raiseRelativeVolume(volDelta, id):
+    url = f"https://api.ws.sonos.com/control/api/v1/players/{id}/playerVolume/relative"
 
     payload = {"volumeDelta": volDelta}
     body = json.dumps(payload)
@@ -55,10 +58,45 @@ async def raiseRelativeVolume(volDelta):
     print(response.text)
     return(response.text)
 
+
+def getVolume(id):
+    import requests
+
+    url = f"https://api.ws.sonos.com/control/api/v1/players/{id}/playerVolume"
+
+    payload = ""
+    headers = {
+        'Content-Type': "application/x-www-form-urlencoded",
+        'X-Sonos-Api-Key': "536e8328-d836-476b-8035-fd206794c9b7",
+        'Host': "api.ws.sonos.com",
+        'Authorization': "Bearer d5c9b056-ce40-454f-8423-35cfbb501742",
+        'Cache-Control': "no-cache",
+        'Postman-Token': "d081d504-024b-42f4-9539-7faa95bc6f1c"
+        }
+
+    response = requests.request("GET", url, data=payload, headers=headers)
+
+    print(response.text)
+    return(response.text)
+
+def fadeInVol():
+    targetVolBathroom = 50
+    raiseBy1 = [raiseRelativeVolume(1, bathroom) for _ in range(targetVolBathroom)]
+    return(raiseBy1)
+
 async def main():
+    # await asyncio.gather(
+    #     lowerVolume(),
+    #     raiseVolume()
+    # )
+    targetVolBathroom = 6
+    targetVolBedroom = 6
     await asyncio.gather(
-        lowerVolume(),
-        raiseVolume()
+        *[raiseRelativeVolume(5, bathroom) for _ in range(0,targetVolBathroom)],
+        *[raiseRelativeVolume(-5, bedroom) for _ in range(0,targetVolBedroom)],
     )
-    
+
+
+
 asyncio.run(main())
+# main()
