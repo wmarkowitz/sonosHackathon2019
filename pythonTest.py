6f5249c993aeb737916f451a328ec72239943562
Sam Markowitz <wmarkowitz@college.harvard.edu>
2019-10-04 20:13:46 -0400

    Volume requests good

--- pythonTest.py
+++ pythonTest.py
@@ -1,21 +1,41 @@
-import http.client
+import requests
+import json
 
-conn = http.client.HTTPConnection("api,ws,sonos,com")
+def lowerVolume():
+    url = "https://api.ws.sonos.com/control/api/v1/players/RINCON_7828CAF4D45E01400/playerVolume"
 
-payload = ""
+    payload = {"volume": 5}
+    body = json.dumps(payload)
+    headers = {
+        'Content-Type': "application/x-www-form-urlencoded",
+        'X-Sonos-Api-Key': "536e8328-d836-476b-8035-fd206794c9b7",
+        'Host': "api.ws.sonos.com",
+        'Authorization': "Bearer d5c9b056-ce40-454f-8423-35cfbb501742",
+        'Cache-Control': "no-cache",
+        'Postman-Token': "e19dae15-21d0-499f-94e5-559953e6bb31"
+        }
 
-headers = {
-    'Content-Type': "application/x-www-form-urlencoded",
-    'X-Sonos-Api-Key': "536e8328-d836-476b-8035-fd206794c9b7",
-    'Host': "api.ws.sonos.com",
-    'Authorization': "Bearer d5c9b056-ce40-454f-8423-35cfbb501742",
-    'Cache-Control': "no-cache",
-    'Postman-Token': "72111493-883b-45ca-8f0c-61a941db35a2"
-    }
+    response = requests.request("POST", url, data=body, headers=headers)
 
-conn.request("POST", "control,api,v1,players,RINCON_7828CAF4D45E01400,playerVolume", payload, headers, body={"volume": 5})
+    print(response.text)
 
-res = conn.getresponse()
-data = res.read()
+def raiseVolume():
+    url = "https://api.ws.sonos.com/control/api/v1/players/RINCON_7828CAE736E601400/playerVolume"
 
-print(data.decode("utf-8"))
+    payload = {"volume": 70}
+    body = json.dumps(payload)
+    headers = {
+        'Content-Type': "application/x-www-form-urlencoded",
+        'X-Sonos-Api-Key': "536e8328-d836-476b-8035-fd206794c9b7",
+        'Host': "api.ws.sonos.com",
+        'Authorization': "Bearer d5c9b056-ce40-454f-8423-35cfbb501742",
+        'Cache-Control': "no-cache",
+        'Postman-Token': "e19dae15-21d0-499f-94e5-559953e6bb31"
+        }
+
+    response = requests.request("POST", url, data=body, headers=headers)
+
+    print(response.text)
+
+lowerVolume()
+raiseVolume()
