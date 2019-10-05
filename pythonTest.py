2cf3c3aba70e616a87003da435dac084294ae64e
Yong Emily Dich <yongemilydich@Yongs-MacBook-Pro.local>
2019-10-04 20:33:37 -0400

    added in relative vol - but might be dangerous, just keep it absolute value otherwise relative vol can get out of control w motion sensor

--- pythonTest.py
+++ pythonTest.py
@@ -38,13 +38,27 @@ async def raiseVolume():
     print(response.text)
     return(response.text)
 
+async def raiseRelativeVolume(volDelta):
+    url = "https://api.ws.sonos.com/control/api/v1/players/RINCON_7828CAE736E601400/playerVolume/relative"
+
+    payload = {"volumeDelta": volDelta}
+    body = json.dumps(payload)
+    headers = {
+        'Content-Type': "application/x-www-form-urlencoded",
+        'X-Sonos-Api-Key': "536e8328-d836-476b-8035-fd206794c9b7",
+        'Host': "api.ws.sonos.com",
+        'Authorization': "Bearer d5c9b056-ce40-454f-8423-35cfbb501742",
+        'Cache-Control': "no-cache",
+        'Postman-Token': "e19dae15-21d0-499f-94e5-559953e6bb31"
+        }
+    response = requests.request("POST", url, data=body, headers=headers)
+    print(response.text)
+    return(response.text)
+
 async def main():
-    # await lowerVolume()
-    # await raiseVolume()
     await asyncio.gather(
         lowerVolume(),
         raiseVolume()
     )
-# lowerVolume()
-# raiseVolume()
+    
 asyncio.run(main())
